ecd9c431c1777916a1dc4ca51ab801ca9462dbfd
vlad <vladisac@gmail.com>
2011-11-03 23:58:52 +0200

    added fieldsets to Company Detail

--- static/app/view/company/Detail.js
+++ static/app/view/company/Detail.js
@@ -13,59 +13,71 @@ Ext.define('INV.view.company.Detail', {
         this.items = [{
                     xtype:'form',
                     border:false,
-                    items:[
-                        {xtype:'textfield', name:'name', fieldLabel: 'Name'},
-                        {xtype:'textfield', name:'vat', fieldLabel: 'VAT'},
-                        {xtype:'textfield', name:'regcom', fieldLabel: 'RegCOM'}
-                    ]
-                },{
-                    xtype: 'fieldcontainer',
-                    fieldLabel: 'Address',
-                    //padding: 10,
-                    width:600,
-                    items: [{
-                            xtype:'inlinegrid',
-                            id: 'address',
-                            store:'CompanyAddresses',
-                            maxWidth:400,
-                            //height:100,
-                            columns:[{dataIndex: 'street', editor: {allowBlank: false}},
-                                {dataIndex: 'city', editor: {allowBlank: false}},
-                                {dataIndex: 'zipcode', editor: {allowBlank: false}}
+                    items:[{xtype:'fieldset',
+                            title: '<p style="font-size:18px">Company</p>',
+                            style: {border:'none'},
+                            border:false,
+                            items: [{xtype:'textfield', name:'name', fieldLabel: 'Name'},
+                                     {xtype:'textfield', name:'vat', fieldLabel: 'VAT'},
+                                     {xtype:'textfield', name:'regcom', fieldLabel: 'RegCOM'}
                             ]
                         }]
-                },{
-                    xtype: 'fieldcontainer',
-                    fieldLabel: 'Contacts',
-                    //padding: 10,
-                    width:600,
-                    items: [{
-                            xtype:'inlinegrid',
-                            id: 'contact',
-                            store:'CompanyContacts',
-                            maxWidth:400,
-                            //height:100,
-                            columns:[{dataIndex: 'name', editor: {allowBlank: false}},
-                                {dataIndex: 'phoneNumber', editor: {allowBlank: false}},
-                                {dataIndex: 'email', editor: {allowBlank: false}}
-                            ]
-                        }]
-                },{
-                    xtype: 'fieldcontainer',
-                    fieldLabel: 'Bank info',
-                    //padding: 10,
-                    width:600,
-                    items: [{
-                            xtype:'inlinegrid',
-                            id: 'bank',
-                            store:'CompanyBanks',
-                            maxWidth:400,
-                            //height:100,
-                            columns:[{dataIndex: 'name', editor: {allowBlank: false}},
-                                {dataIndex: 'iban', editor: {allowBlank: false}}
-                            ]
-                        }]
-                }
+                    },{
+                        xtype:'fieldset',
+                        title: '<p style="font-size:18px">Address, Contact, Bank</p>',
+            collapsible:true,
+                        style: {border:'none'},
+                        border:false,
+                        items: [{
+                                xtype: 'fieldcontainer',
+                                fieldLabel: 'Address',
+                                //padding: 10,
+                                width:600,
+                                items: [{
+                                        xtype:'inlinegrid',
+                                        id: 'address',
+                                        store:'CompanyAddresses',
+                                        maxWidth:400,
+                                        //height:100,
+                                        columns:[{dataIndex: 'street', editor: {allowBlank: false}},
+                                            {dataIndex: 'city', editor: {allowBlank: false}},
+                                            {dataIndex: 'zipcode', editor: {allowBlank: false}}
+                                        ]
+                                    }]
+                            },{
+                                xtype: 'fieldcontainer',
+                                fieldLabel: 'Contacts',
+                                //padding: 10,
+                                width:600,
+                                items: [{
+                                        xtype:'inlinegrid',
+                                        id: 'contact',
+                                        store:'CompanyContacts',
+                                        maxWidth:400,
+                                        //height:100,
+                                        columns:[{dataIndex: 'name', editor: {allowBlank: false}},
+                                            {dataIndex: 'phoneNumber', editor: {allowBlank: false}},
+                                            {dataIndex: 'email', editor: {allowBlank: false}}
+                                        ]
+                                    }]
+                            },{
+                                xtype: 'fieldcontainer',
+                                fieldLabel: 'Bank info',
+                                //padding: 10,
+                                width:600,
+                                items: [{
+                                        xtype:'inlinegrid',
+                                        id: 'bank',
+                                        store:'CompanyBanks',
+                                        maxWidth:400,
+                                        //height:100,
+                                        columns:[{dataIndex: 'name', editor: {allowBlank: false}},
+                                            {dataIndex: 'iban', editor: {allowBlank: false}}
+                                        ]
+                                    }]
+                            }]}
+
+
             ];
         this.callParent(arguments);
     },
